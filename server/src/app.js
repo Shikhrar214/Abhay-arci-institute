26949fd0cdcd694513f852ceb8fda42be4765e5f
Shikhar Srivastav <shikharsrivastav9450@gmail.com>
2026-03-03 23:32:30 +0530

    increasing limit of form data in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 
-app.use(express.json({limit: "20kb"}));
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
